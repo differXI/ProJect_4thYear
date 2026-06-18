d2426fcae88a204201ee8c9de2b9d835989fb222
Thanachai <thanachai0948098172@gmail.com>
2026-06-18 17:01:01 +0700

    debug on permission cros

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,22 +25,13 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-if settings.cors_origins:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.cors_origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-else:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=False,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api")
 
